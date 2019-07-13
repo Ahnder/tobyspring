5c5edb1d49bbc16dadee0018752f23b84f1164a9
Ahnder <skguss1@gmail.com>
2019-07-13 20:58:59 +0900

    Factory IoC

--- src/user/dao/DaoFactory.java
+++ src/user/dao/DaoFactory.java
@@ -1,11 +1,29 @@
 package user.dao;
 
 public class DaoFactory {
-    public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
 
-        UserDao userDao = new UserDao(connectionMaker);
+    // DConnectionMaker() 는 언제든지 다른걸로 유연하게 바꿀 수 있다
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
+
+    /*public UserDao userDao() {
+        UserDao userDao = new UserDao(connectionMaker());
 
         return userDao;
+    }*/
+
+    // 위의 userDao() 는 아래의 코드로 대체 가능
+    public UserDao userDao() {
+        return new UserDao(connectionMaker());
     }
+
+    // 밑의 accountDao(), messageDao() 처럼 다른 DAO 생성 기능도 간단하게 추가 가능하다
+    /*public AccountDao accountDao() {
+        return new AccountDao(connectionMaker());
+    }*/
+
+    /*public MessageDao messageDao() {
+        return new MessageDao(connectionMaker());
+    }*/
 }
