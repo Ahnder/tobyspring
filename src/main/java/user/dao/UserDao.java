c323d057d7e6b1be8b011bea3762eaba9043a65e
Ahnder <skguss1@gmail.com>
2019-07-16 21:28:22 +0900

    2.3.2 junit test consistency

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -58,4 +58,34 @@ public class UserDao {
         return user;
     }
 
+    // deleteAll() 메서드 : 테이블의 모든 레코드를 삭제
+    public void deleteAll() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("delete from users");
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    // getCount() 메서드 : 테이블의 레코드 개수를 반환
+    public int getCount() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+
+    }
+
 }
--- src/main/java/user/dao/UserDaoTest.java
+++ src/main/java/user/dao/UserDaoTest.java
@@ -11,10 +11,15 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 
-// 2.2.2 JUnit 테스트로 전환
+// 2.3.2 테스트 결과의 일관성
+// 이전까지의 UserDaoTest의 문제는 이전 테스트 때문에 DB에 등록된
+// 중복 데이터가 있을 수 있다는 점이다
+// 이를 개선하기 위해 deleteAll() 과 getCount() 메서드를 추가한다(UserDao 클래스에 메서드 추가)
+
 
 public class UserDaoTest {
 
+    // addAndGet() 테스트를 확장(deleteAll(), getCount())
     @Test
     public void addAndGet() throws SQLException {
 
@@ -23,6 +28,10 @@ public class UserDaoTest {
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        //deleteAll() 메서드로 테이블을 비우고 getCount()로 테이블수가 0이 맞는지 확인
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
         User user = new User();
 
         user.setId("gyumee");
@@ -30,6 +39,9 @@ public class UserDaoTest {
         user.setPassword("park");
 
         dao.add(user);
+        //테이블에 레코드가 1개 추가 되었으니 getCount() 가 1이 맞는지 확인
+        //getCount()의 기능을 위 테스트와 비교 각각 0과 1이 잘 나오면 검증 통과
+        assertThat(dao.getCount(), is(1));
 
         User user2 = dao.get(user.getId());
 
