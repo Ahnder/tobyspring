df9a209ac39ef317d9857df85170600911538ec0
Ahnder <skguss1@gmail.com>
2019-07-16 21:12:02 +0900

    junit test first

--- src/main/java/user/dao/UserDaoTest.java
+++ src/main/java/user/dao/UserDaoTest.java
@@ -1,50 +1,41 @@
 package user.dao;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-// 2.2.1 테스트 검증의 자동화(Improvement of UserDaoTest, Print a success or failure message)
-// 테스트 확인 사항 : add()에 전달한 User 오브젝트에 담긴 사용자 정보와
-// get()을 통해 다시 DB에서 가져온 User 오브젝트의 정보가 서로 정확히 일치하는지 확인
-// 정확히 일치한다면 모든 정보가 빠짐없이 DB에 등록됐고, 이를 다시
-// DB에서 정확히 가져왔다는 사실을 알 수 있다
-//
-// 개선된 코드는 get()에서 가져온 결과를 콘솔에 출력하여 확인하는 방식을
-// "테스트 성공"과 "테스트 실패"라는 메세지가 각각 출력하는 방식으로 작성함
+
+// 2.2.2 JUnit 테스트로 전환
+
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException {
-        ApplicationContext context =
-                new GenericXmlApplicationContext("applicationContext.xml");
+
+    @Test
+    public void addAndGet() throws SQLException {
+
+        ApplicationContext context = new GenericXmlApplicationContext(
+                "applicationContext.xml");
+
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("park");
 
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
-
         User user2 = dao.get(user.getId());
 
-        //System.out.println(user2.getName());
-        //System.out.println(user2.getPassword());
-        //System.out.println(user2.getId() + " 조회 성공");
-        // 위 3줄의 코드를 개선
-        if (!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 : (do not match - name)");
-        } else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 : (do not match - password)");
-        } else {
-            System.out.println("조회 테스트 성공");
-        }
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
 
     }
-
 }
 
