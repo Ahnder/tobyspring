c39936bb5788d5c2acc960e55e1d7a3a3c725f63
Ahnder <skguss1@gmail.com>
2019-07-18 20:16:08 +0900

    application of Autowired, Runwith, ContextConfiguration, test_...xml

--- src/main/java/user/dao/UserDaoTest.java
+++ src/main/java/user/dao/UserDaoTest.java
@@ -2,9 +2,14 @@ package user.dao;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import user.domain.User;
 
 import java.sql.SQLException;
@@ -12,47 +17,48 @@ import java.sql.SQLException;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-
-// 2.3.5 테스트 코드 개선
-// 테스트 코드에서 중복된 코드를 JUnit @Before 을 사용
-// * JUnit이 하나의 테스트 클래스를 가져와 테스트를 수행하는 방식 간단 정리
-// 1. 테스트 클래스에서 @Test가 붙은 public이고 void형이며 파라미터가 없는
-//    테스트 메서드를 모두 찾는다
-// 2. 테스트 클래스의 오브젝트를 하나 만든다
-// 3. @Before가 붙은 메서드가 있으면 실행한다
-// 4. @Test가 붙은 메서드를 하나 호출하고 테스트 결과를 저장해둔다
-// 5. @After가 붙은 메서드가 있으면 실행한다
-// 6. 나머지 테스트 메서드에 대해 2~5번을 반복한다
-// 7. 모든 테스트의 결과를 종합하여 돌려준다
-// ** 각 테스트 메서드를 실행할 때마다 테스트 클래스의 오브젝트를 새로 만든다
-//    한번 만들어진 테스트 클래스의 오브젝트는 하나의 테스트 메서드를 사용하고 나면 버려진다
-//    - 테스트 메서드 실행마다 새로운 오브젝트를 만드는 이유
-//      : 각 테스트가 서로 영향을 주지 않고 독립적으로 실행됨을 보장해주기 위해
+// 2.4 스프링 테스트 적용
+// 2.4.1 테스트를 위한 애플리케이션 컨텍스트 관리
+// - 스프링 테스트 컨텍스트 프레임워크 적용
+//   @Before 메서드에서 애플리케이션 컨텍스트를 생성하는 코드르 제거하고
+//   @Autowired 애너테이션과 @Runwith, @ContextConfiguration 애너테이션을 사용한다
+@RunWith(SpringJUnit4ClassRunner.class) // 스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
+@ContextConfiguration(locations = "/test_applicationContext.xml") // 테스트 컨텍스트가 자동으로 만들어줄
+// 애플리케이션 컨텍스트의 위치 지정, test전용 설정파일을 연결
 public class UserDaoTest {
+    @Autowired
+    private ApplicationContext context;
 
-    // setUp() 메서드에서 만드는 오브젝트를 테스트 메서드에서
-    // 사용할 수 있도록 인스턴스 변수로 선언한다
+    @Autowired
     private UserDao dao;
 
-    // 2개의 테스트 메서드에서 반복되는 User 픽스쳐들도 앞으로의 테스트 메서드에서
-    // 쓰일 가능성이 높으므로 user1등은 인스턴스 변수를 선언하고
-    // 오브젝트 생성은 @Before 에서 진행 시킨다
     private User user1;
     private User user2;
     private User user3;
 
-    // JUnit이 제공하는 애너테이션, @Test 메서드가 실행되기 전에
-    // 먼저 실행돼야 하는 메서드를 정의한다
     @Before
     public void setUp() {
-        ApplicationContext context = new GenericXmlApplicationContext(
-                "applicationContext.xml");
 
-        this.dao = context.getBean("userDao", UserDao.class);
+        this.dao = this.context.getBean("userDao", UserDao.class);
 
         this.user1 = new User("gyumee", "박성철", "park");
         this.user2 = new User("leegw", "이길원", "lee");
         this.user3 = new User("bumjin", "박범진", "jin");
+
+        // 테스트 메서드의 컨텍스트 공유 확인용 코드
+        //  출력된 context와 this의 오브젝트 값을 잘 살펴보면, context는 세 번 모두 동일하다
+        // 따라서 하나의 애플리케이션 컨텍스트가 만들어져 모든 테스트 메서드에서 사용되고
+        // 있음을 알 수 있다
+        //  반면에 UserDaoTest의 오브젝트는 매번 주소 값이 다르다
+        // JUnit이 테스트 메서드를 실행할 때마다 새로운 테스트 오브젝트를 만들기 때문이다
+        //  그렇다면 context 변수에 어떻게 애플리케이션 컨텍스트가 들어 있는 것일까
+        // 스프링의 JUnit 확장기능은 테스트가 실행되기 전에 딱 한 번만 애플리케이션 컨텍스트를
+        // 만들어두고, 테스트 오브젝트가 만들어질 때마다 특별한 방법을 이용해
+        // 애플리케이션 컨텍스트 자신을 테스트 오브젝트의 특정 필드에 주입해주는 것이다
+
+        System.out.println(this.context);
+        System.out.println(this);
+        //
     }
 
     @Test
