b89f0fe1f54057e6024d7c4c031d2537dc9b2bb6
Ahnder <skguss1@gmail.com>
2019-07-19 17:27:46 +0900

    3.1.1 apply to try~catch~finally

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -62,33 +62,101 @@ public class UserDao {
         return user;
     }
 
+    // 3.1.1 예외처리 기능을 갖춘 DAO
+    // try문 적용
+    //  이 메서드에서는 Connection과 PreparedStatement라는 두 개의 공유 리소스를 가져와서
+    // 사용한다. 정상적으로 처리되면 메서드를 마치기전 각각 close()를 호출, 리소스를 반환한다
+    //  하지만 PreparedStatement를 처리하는 중에 예외가 발생하면 메서드 실행을 끝마치지 못하고
+    // 바로 메서드를 빠져나가게 된다
+    //  이때 문제는 Connection과 PrearedStatement의 close() 메서드가 실행되지 않아서
+    // 제대로 리소스가 반환되지 않을 수 있다
+
     // deleteAll() 메서드 : 테이블의 모든 레코드를 삭제
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
 
-        PreparedStatement ps = c.prepareStatement("delete from users");
+        try {
+            // 예외가 발생할 가능성이 있는 코드를 모두 try 블럭으로 묶어준다
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
 
-        ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e; // 예외가 발생했을 때 부가적인 작업을 해줄 수 있도록 catch블록을 둔다
+                     // 아직은 예외를 다시 메서드 밖으로 던지는 것 밖에 없다
 
-        ps.close();
-        c.close();
+        } finally {
+            // finally이므로 try 블록에서 예외가 발생했을 때나 안 했을 때나 모두 실행된다
+            if (ps != null) {
+                try {
+                    ps.close();
+
+                } catch (SQLException e) {
+                    // ps.close() 메서드에서도 SQLException이 발생할 수 있기 때문에
+                    // 이를 잡아줘야 한다
+                    // 그렇지 않으면 Connection을 close() 하지 못하고 메서드를 빠져나갈 수 있다
+                }
+            } // if (ps != null){} 끝
+
+            if (c != null) {
+                try {
+                    c.close();
+
+                } catch (SQLException e) {
+
+                }
+            }
+        }
+
+
     }
 
     // getCount() 메서드 : 테이블의 레코드 개수를 반환
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
 
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
+            return rs.getInt(1);
 
-        rs.close();
-        ps.close();
-        c.close();
+        } catch (SQLException e) {
+            throw e;
 
-        return count;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+
+                } catch (SQLException e) {
+
+                }
+            } // if(rs != null){} 끝
+
+            if (ps != null) {
+                try {
+                    ps.close();
+
+                } catch (SQLException e) {
+
+                }
+            } // if(ps != null){} 끝
+
+            if (c != null) {
+                try {
+                    c.close();
+
+                } catch (SQLException e) {
+
+                }
+            } // if(c != null){} 끝
+        } // try~catch~finally문 끝
 
     }
 
