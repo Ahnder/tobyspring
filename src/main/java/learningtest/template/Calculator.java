1fa6748edb05ca5ae750160a00a05ca939353d2b
Ahnder <skguss1@gmail.com>
2019-07-21 21:56:11 +0900

    apply to JdbcTemplate : update(), queryForObject(Integer)

---
+++ src/main/java/learningtest/template/Calculator.java
@@ -0,0 +1,93 @@
+package learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculator {
+    public Integer calcSum(String filepath) throws IOException {
+        LineCallback sumCallback =
+                new LineCallback() {
+                    public Integer doSomethingWithLine(String line, Integer value) {
+
+                        return value + Integer.valueOf(line);
+
+                    }
+                };
+
+                return lineReadTemplate(filepath, sumCallback, 0);
+
+    } // calcSum() end
+
+    public Integer calcMultiply(String filepath) throws IOException {
+        LineCallback multiplyCallback =
+                new LineCallback() {
+                    public Integer doSomethingWithLine(String line, Integer value) {
+
+                        return value * Integer.valueOf(line);
+                    }
+                };
+
+        return lineReadTemplate(filepath, multiplyCallback, 1);
+
+    } // calcMultiply() end
+
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback)
+            throws IOException {
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            int ret = callback.doSomethingWithReader(br);
+
+            return ret;
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+
+                }
+            }
+        }
+    } // fileReadTemplate() end
+
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int
+                                    initVal) throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer res = initVal;
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                res = callback.doSomethingWithLine(line, res);
+
+            }
+            return res;
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+
+                }
+            }
+        }
+    } // lineReadTemplate() end
+
+}
--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -1,6 +1,7 @@
 package user.dao;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import user.domain.User;
 
 import javax.sql.DataSource;
@@ -9,36 +10,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-// UserDao를 분리된 JdbcContext를 DI 받아서 사용할 수 있게 리팩토링
 public class UserDao {
     private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+
         this.dataSource = dataSource;
     }
 
-    private JdbcContext jdbcContext;
-
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
-
     public void add(final User user) throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(
-                new StatementStrategy() {
-                    public PreparedStatement makePreparedStatement(Connection c)
-                            throws SQLException {
-                        PreparedStatement ps =
-                                c.prepareStatement("insert into users(id, name," +
-                                        "password) values(?, ?, ?)");
-                        ps.setString(1, user.getId());
-                        ps.setString(2, user.getName());
-                        ps.setString(3, user.getPassword());
-
-                        return ps;
-                    }
-                }
-        );
+        this.jdbcTemplate.update("insert into users(id, name, password) value(?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
     }
 
     public User get(String id) throws SQLException {
@@ -73,55 +57,28 @@ public class UserDao {
 
     // deleteAll() 메서드 : 테이블의 모든 레코드를 삭제
     public void deleteAll() throws SQLException {
-        this.jdbcContext.executeSql("delete from users");
+        this.jdbcTemplate.update("delete from users");
 
     }
 
     // getCount() 메서드 : 테이블의 레코드 개수를 반환
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        // queryForInt()를 사용한 코드
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
 
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-            rs = ps.executeQuery();
-            rs.next();
-
-            return rs.getInt(1);
-
-        } catch (SQLException e) {
-            throw e;
-
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-
-                } catch (SQLException e) {
-
-                }
-            } // if(rs != null){} 끝
-
-            if (ps != null) {
-                try {
-                    ps.close();
-
-                } catch (SQLException e) {
-
-                }
-            } // if(ps != null){} 끝
-
-            if (c != null) {
-                try {
-                    c.close();
-
-                } catch (SQLException e) {
-
-                }
-            } // if(c != null){} 끝
-        } // try~catch~finally문 끝
+        // 밑의 코드를 간략화 시킨 코드가 위의 queryForInt()를 사용한 코드이다
+        /*return this.jdbcTemplate.query(new PreparedStatementCreator() {
+            public PreparedStatement createPreparedStatement(Connection con)
+                    throws SQLException {
+                return con.prepareStatement("select count(*) from users");
+            }
+        }, new ResultSetExtractor<Integer>() {
+            public Integer extractData(ResultSet rs) throws SQLException,
+                    DataAccessException {
+                rs.next();
+                return rs.getInt(1);
+            }
+        });*/
 
     }
 
