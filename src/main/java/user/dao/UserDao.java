f091f73372589294a1d04223aa16622f21ad1f62
Ahnder <skguss1@gmail.com>
2019-07-22 22:49:42 +0900

    clean up the code for DI

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -10,14 +10,24 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
-    private DataSource dataSource;
-    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-
-        this.dataSource = dataSource;
     }
+
+    private JdbcTemplate jdbcTemplate;
+
+    private RowMapper<User> userMapper =
+            new RowMapper<User>() {
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    User user = new User();
+                    user.setId(rs.getString("id"));
+                    user.setName(rs.getString("name"));
+                    user.setPassword(rs.getString("password"));
+
+                    return user;
+                }
+            }; // userMapper end
 
     public void add(final User user) throws SQLException {
         this.jdbcTemplate.update("insert into users(id, name, password) value(?, ?, ?)",
@@ -27,18 +37,7 @@ public class UserDao {
     public User get(String id) throws SQLException {
 
         return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-                new Object[]{id},
-                new RowMapper<User>() {
-                    public User mapRow(ResultSet rs, int rowNum)
-                            throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-
-                        return user;
-                    }
-                });
+                new Object[]{id}, this.userMapper);
     }
 
     // deleteAll() 메서드 : 테이블의 모든 레코드를 삭제
@@ -52,36 +51,12 @@ public class UserDao {
         // queryForInt()를 사용한 코드
         return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
 
-        // 밑의 코드를 간략화 시킨 코드가 위의 queryForInt()를 사용한 코드이다
-        /*return this.jdbcTemplate.query(new PreparedStatementCreator() {
-            public PreparedStatement createPreparedStatement(Connection con)
-                    throws SQLException {
-                return con.prepareStatement("select count(*) from users");
-            }
-        }, new ResultSetExtractor<Integer>() {
-            public Integer extractData(ResultSet rs) throws SQLException,
-                    DataAccessException {
-                rs.next();
-                return rs.getInt(1);
-            }
-        });*/
-
     } // getCount() end
 
     public List<User> getAll() {
 
         return this.jdbcTemplate.query("select * from users order by id",
-                new RowMapper<User>() {
-                    public User mapRow(ResultSet rs, int rowNum)
-                            throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-
-                        return user;
-                    }
-                });
+                this.userMapper);
     } // getAll() end
 
 }
